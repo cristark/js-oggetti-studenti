17ac52619d0a14be35b3088f4653e248a811e748
Cristiano <cristiano.tarconi@gmail.com>
2021-01-15 17:50:34 +0100

    objects + array js logic console log

--- js/script.js
+++ js/script.js
@@ -6,3 +6,39 @@ Stampare a schermo attraverso il for in tutte le proprietà.
 
 .Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età. */
 
+// Creo oggetto studente
+var studente = {
+    nome : 'Antonio',
+    cognome : 'Spaziani',
+    eta : 34
+};
+
+// Stampo in console le proprietà dello studente
+for (var k in studente) {
+    console.log(`${k}: ${studente[k]}`);
+}
+
+// Creo array di oggetti formato dalle informazioni di più studenti
+var studenti = [
+    {
+        nome : 'Antonio',
+        cognome : 'Spaziani',
+        eta : 34
+    },
+    {
+        nome : 'Alessia',
+        cognome : 'Tesi',
+        eta : 28
+    },
+    {
+        nome : 'Valeria',
+        cognome : 'Rossi',
+        eta : 25
+    },
+];
+
+// Stampo in console nome e cognome di tutti gli studenti nell'array precedente
+for (var i = 0; i < studenti.length; i++) {
+    console.log(`nome: ${studenti[i].nome}`);
+    console.log(`cognome: ${studenti[i].cognome}`);
+}
